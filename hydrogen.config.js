27f68cc5fda829f448ba2afa6a90ef01b21e0b75
Nicholas Steinbauer <web@obrien.com>
2023-06-23 16:10:41 -0400

    trying environment variables

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -2,11 +2,11 @@ import {defineConfig} from '@shopify/hydrogen/config';
 
 export default defineConfig({
   shopify: {
-    storeDomain: 'boathouse-cart-rental-2023.myshopify.com',
-    storefrontToken: '1df896d8e460879b8537f9a0aeba1b22',
-    storefrontApiVersion: '2022-07',
-  },
-  logger: {
-    showQueryTiming: true,
+    defaultCountryCode: 'US',
+    defaultLanguageCode: 'EN',
+    storeDomain: Oxygen?.env?.PUBLIC_STORE_DOMAIN,
+    storefrontToken: Oxygen?.env?.PUBLIC_STOREFRONT_API_TOKEN,
+    storefrontApiVersion: Oxygen?.env?.PUBLIC_STOREFRONT_API_VERSION
   }
+
 });
